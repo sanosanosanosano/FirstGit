e5d552586932079f2d9a659b390e8f33cb3015a5
g13940sm <g13940sm@s30443.tsuda.ac.jp>
2015-04-10 11:11:20 +0900

    FirstHelloWorldです

---
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthelloworld;
+
+/**
+ *
+ * @author g13940sm
+ */
+public class FirstHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+}
